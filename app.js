8e4095875d4ccb65121c8b1af543add476735ca4
Felipe Carminatti <felipecarminatti28@gmail.com>
2022-10-18 17:54:46 -0300

    Atualização2

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 let btn = document.querySelector('[btn-click]')
 let aside = document.getElementById('aside')
+let conteudo = document.getElementById('conteudo')
 
 btn.addEventListener('click', function () {
     aside.classList.toggle('efeitoMenu')
 })
+
+function ano() {
+    let anoAtual = document.getElementById('ano')
+    let data = new Date()
+    let ano = data.getFullYear()
+    anoAtual.innerHTML = ano
+}
+ano()
