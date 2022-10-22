bf87b95780b55aa0ab9a853a26b6f085e31c07b8
Felipe Carminatti <felipecarminatti28@gmail.com>
2022-10-22 16:57:08 -0300

    Atualização4

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ btn.addEventListener('click', function () {
     aside.classList.toggle('efeitoMenu')
 })
 
+let links = document.querySelectorAll('#btn-link')
+links.forEach(link => {
+    link.onclick = function () {
+        aside.classList.remove('efeitoMenu')
+    }
+})
+
 function ano() {
     let anoAtual = document.getElementById('ano')
     let data = new Date()
@@ -16,7 +23,7 @@ ano()
 
 let scroll = document.getElementById('scroll')
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 400){
+    if(window.pageYOffset > 500){
         scroll.style.display = 'block'
     }else{
         scroll.style.display = 'none'
@@ -25,5 +32,23 @@ window.addEventListener('scroll', () => {
 })
 
 scroll.addEventListener('click', () => {
-    window.scrollTo(0, 0)
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+let article1 = document.getElementById('article1')
+let article2 = document.getElementById('article2')
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 500){
+        article1.style.transition = 'all 1s'
+        article1.style.transform = 'translate(0px)'
+        article1.style.opacity = 1
+    }if (window.pageYOffset > 800) {
+        article2.style.transition = 'all 1s'
+        article2.style.transform = 'translate(0px)'
+        article2.style.opacity = 1
+    }
 })
